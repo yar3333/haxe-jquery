54f76f27be6b8a3fc60f51e77a82c7a09b0a228a
Yaroslav Sivakov <yar3333@gmail.com>
2013-11-12 12:02:18 +0400

    + ImgLiquid jquery plugin

---
+++ library/js/jquery/ImgLiquid.hx
@@ -0,0 +1,29 @@
+package js.jquery;
+
+/**
+ *	html5 data attr (overwrite all)
+ *			data-imgLiquid-fill="true"
+ *			data-imgLiquid-horizontalAlign="center"
+ *			data-imgLiquid-verticalAlign='center'
+ */
+
+class ImgLiquid
+{
+	static function __init__() : Void
+	{
+		haxe.macro.Compiler.includeFile("js/jquery/ImgLiquid.js");
+	}
+	
+	public static inline function imgCenter(jq:js.JQuery, ?params : { 
+		?fill : Bool,
+		?verticalAlign : String,	// 'center' //	'top'	//	'bottom' // '50%'  // '10%' 
+		?horizontalAlign : String,	// 'center' //	'left'	//	'right'  // '50%'  // '10%' 
+		?onStart : Void->Void,
+		?onFinish : Void->Void,
+		?onItemStart : Int->js.JQuery->js.JQuery->Void, // function(index, container, img)
+		?onItemFinish : Int->js.JQuery->js.JQuery->Void, // function(index, container, img)
+	}) : js.JQuery untyped
+	{
+		return jq.imgLiquid(params);
+	}
+}
