bdaef9fdd21d8bc9f8b5605a46b16e768746b1aa
Yaroslav Sivakov <yar3333@gmail.com>
2013-11-18 16:56:31 +0400

    + jquery: autoresize plugin

---
+++ library/js/jquery/Autoresize.hx
@@ -0,0 +1,24 @@
+package js.jquery;
+
+typedef AutoresizeOptions = 
+{
+	@:optional var onResize : Void->Void;
+	@:optional var animate : Bool;
+	@:optional var animateDuration : Int;
+	@:optional var animateCallback : Void->Void;
+	@:optional var extraSpace : Int;
+	@:optional var limit: Int;
+}
+
+class Autoresize
+{
+	static function __init__() : Void
+	{
+		haxe.macro.Compiler.includeFile("js/jquery/Autoresize.js");
+	}
+	
+	public static function autoresize(jq:js.JQuery, ?params:AutoresizeOptions) : js.JQuery
+	{
+		return jq.autoreize(params);
+	}
+}
